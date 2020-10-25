45b6ce1af6092dacd7abce2438781d05e42ef1a6
Igor1101 <igor.muravyov.2020@ukr.net>
2020-10-25 14:18:32 +0200

    reconfigure to use SysTick

--- generic/main_app.c
+++ generic/main_app.c
@@ -1,5 +1,8 @@
 #include "stm32f4xx_hal.h"
-
+#include "usart.h"
+#include "gpio.h"
+#include "iwdg.h"
+#include "generic/serial.h"
 extern void SystemClock_Config(void);
 
 int main(void)
@@ -15,5 +18,14 @@ int main(void)
 	MX_USART1_UART_Init();
 	MX_USART2_UART_Init();
 	while (1) {
+		// reset watchdog
+		__HAL_IWDG_RELOAD_COUNTER(&hiwdg);
+		// goto sleep
+		HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
 	}
+}
+// interrupt handlers
+void SysTick_Handler(void)
+{
+	pr_debugln("systick handler");
 }
